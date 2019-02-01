d13a6db480f79a1e46d0778d897872e0e107f5af
Zukky_13 <www.kase@gmail.com>
2019-02-01 17:58:12 +0900

    Playerをボールにするかどうかで迷っている最中

--- Assets/StageScene/Scripts/Others/Test.cs
+++ Assets/StageScene/Scripts/Others/Test.cs
@@ -4,12 +4,63 @@ using UnityEngine;
 
 namespace ReviewGames
 {
+    [RequireComponent(typeof(Rigidbody))]
     public class Test : MonoBehaviour
     {
+        /// <summary>移動速度を調整するパラメータ</summary>
+        [Header("Parameters")]
+        [SerializeField] protected float m_moveSpeed = 5f;
+        /// <summary>プレイヤーが回転する速度</summary>
+        [SerializeField] float m_turnSpeed = 1f;
+        /// <summary>縦軸入力</summary>
+        float m_horizontal;
+        /// <summary>横軸入力</summary>
+        float m_vertical;
+
+        /// <summary>操作の標準とする向き</summary>
+        [Header("Components")]
+        [SerializeField] Transform m_directionalStandard;
+        /// <summary>アタッチされているRigidbody</summary>
+        Rigidbody m_rb;
+
+
+        private void OnEnable()
+        {
+            m_rb = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            //方向の入力を取得する
+            m_horizontal = Input.GetAxis("Horizontal");
+            m_vertical = Input.GetAxis("Vertical");
+        }
 
         private void FixedUpdate()
         {
-            //Debug.Log(Physics.gravity);
+            Move();
+        }
+
+        /// <summary>
+        /// Movement
+        /// </summary>
+        private void Move()
+        {
+            // x-z 平面(地面と平行)の速度を求める
+            var dir = new Vector3(m_horizontal, 0, m_vertical); // 方向の入力で、x-z平面の移動方向が決まる。
+
+            // 入力に合わせてアニメーションを制御する
+            if (dir != Vector3.zero) // 移動している時
+            {
+                dir = m_directionalStandard.TransformDirection(dir);//カメラに対して正面の向きに変換する
+                dir.y = 0;
+                transform.forward = Vector3.Slerp(transform.forward, dir, m_turnSpeed); // 入力された向きに対して少し遅延しながら入力方向に向かせる
+                m_rb.velocity = dir * m_moveSpeed;
+            }
+            else
+            {
+                m_rb.velocity = Vector3.zero;
+            }
         }
     }
 }
--- Assets/StageScene/Scripts/Player/PlayerController.cs
+++ Assets/StageScene/Scripts/Player/PlayerController.cs
@@ -7,23 +7,32 @@ namespace ReviewGames
     [RequireComponent(typeof(Rigidbody))]
     public class PlayerController : MonoBehaviour
     {
+        #region Properties
+        /// <summary>Input direction. コントローラーから入力されたベクトル</summary>
+        private Vector3 InputDir { get { return new Vector3(m_horizontal, 0, m_vertical); } }
+        #endregion
+
         #region Field
         /// <summary>移動速度を調整するパラメータ</summary>
         [Header("Parameters")]
         [SerializeField] protected float m_moveSpeed = 5f;
         /// <summary>プレイヤーが回転する速度</summary>
         [SerializeField] float m_turnSpeed = 8f;
+        /// <summary>身体の正面をなめらかに入力軸方向に向ける為の内挿量</summary>
+        [SerializeField] float m_turnInterpolateAmount = 1f;
         /// <summary>ジャンプ力を調整するパラメータ</summary>
         [SerializeField] float m_jumpPower = 10f;
         // 重力加速度を調整するパラメーター
         [SerializeField] float m_gravityMultiplier = 1f;
-
+        /// <summary>縦軸入力</summary>
         float m_horizontal;
+        /// <summary>横軸入力</summary>
         float m_vertical;
 
-        /// <summary>操作の標準とする向き</summary>
+        /// <summary>操作の標準とする向き(基本メインカメラ)</summary>
         [Header("Components")]
         [SerializeField] Transform m_directionalStandard;
+        /// <summary>アタッチされているRigidbody</summary>
         Rigidbody m_rb;
         /// <summary>浮動ジョイスティック</summary>
         [SerializeField] FloatingJoystick m_FJoyStick;
@@ -37,21 +46,33 @@ namespace ReviewGames
             m_animator = GetComponent<Animator>();
         }
 
+        private void Update()
+        {
+            //方向の入力を取得する
+            m_horizontal = (Input.GetAxis("Horizontal") == 0f) ? m_FJoyStick.Horizontal : Input.GetAxis("Horizontal"); //方向キーの入力が無い時はジョイスティックから入力をとる
+            m_vertical = (Input.GetAxis("Vertical") == 0f) ? m_FJoyStick.Vertical : Input.GetAxis("Vertical"); // 方向キーの入力が無い時はジョイスティックから入力をとる
+        }
 
+        private void FixedUpdate()
+        {
+            Move();
+            //Turn();
+        }
 
         /// <summary>
-        /// Movement
+        /// 入力に対する移動処理.
         /// </summary>
         private void Move()
         {
             // x-z 平面(地面と平行)の速度を求める
-            var dir = new Vector3(m_horizontal, 0, m_vertical); // 方向の入力で、x-z平面の移動方向が決まる。
+            var dir = InputDir; // 方向の入力で、x-z平面の移動方向が決まる。
 
             // 入力に合わせてアニメーションを制御する
             if (dir != Vector3.zero) // 移動している時
             {
                 dir = m_directionalStandard.TransformDirection(dir);//カメラに対して正面の向きに変換する
-                transform.forward = Vector3.Slerp(transform.forward, dir, m_turnSpeed); // 入力された向きに対して少し遅延しながら入力方向に向かせる
+                dir.y = 0;
+                transform.forward = Vector3.Slerp(transform.forward, dir, m_turnInterpolateAmount); // 入力された向きに対して少し遅延しながら入力方向に向かせる
                 m_rb.velocity = dir * m_moveSpeed;
 
                 m_animator.SetFloat(AnimParameter.Speed.ToString(), dir.sqrMagnitude); // Walk or Run へ遷移
@@ -59,7 +80,10 @@ namespace ReviewGames
             else // 移動していない時
             {
                 m_animator.SetFloat(AnimParameter.Speed.ToString(), 0f); //Idleへ遷移させる
-                m_rb.velocity = Vector3.zero;
+                if (m_animator.GetBool(AnimParameter.IsGrounded.ToString())) // 足が何かの面に着いている時は入力が無くなったら即時playerの動きを止める
+                {
+                    m_rb.velocity = Vector3.zero;
+                }
             }
         }
 
@@ -72,18 +96,7 @@ namespace ReviewGames
             transform.rotation = yAxisRot * transform.rotation;  // 元の回転値と合成して上書き
         }
 
-        private void Update()
-        {
-            //方向の入力を取得する
-            m_horizontal = (Input.GetAxisRaw("Horizontal") == 0f) ? m_FJoyStick.Horizontal : Input.GetAxisRaw("Horizontal"); //方向キーの入力が無い時はジョイスティックから入力をとる
-            m_vertical = (Input.GetAxis("Vertical") == 0f) ? m_FJoyStick.Vertical : Input.GetAxisRaw("Vertical"); // 方向キーの入力が無い時はジョイスティックから入力をとる
-        }
 
-        private void FixedUpdate()
-        {
-            Move();
-            //Turn();
-        }
 
         /// <summary>
         /// animator state machineのステート一覧
